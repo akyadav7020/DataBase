c1f535bef18e02df6fb7f1109049b830c2b91557
abhay <akyadav7020@gmail.com>
2022-09-14 14:30:08 +0530

    saved

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def main():
             count = int(request.form['num'])
             details = []
             ch_name="Abhay"
-            data1=[{"V_link":"a","Likes":100,"Title":"title1","thumbnail":"thumbnail_url1","Views":500},
-                   {"V_link":"b","Likes":200,"Title":"title2","thumbnail":"thumbnail_url2","Views":100}]
+            video_id = vd.get_id_of_videos(ch_link, count)
+            data1=[{"V_link":video_id,"Likes":100,"Title":"title1","thumbnail":"thumbnail_url1","Views":500}]
             """video_id = vd.get_id_of_videos(ch_link,count)
             if (len(video_id)) == 0:
                 return "Invalid Link Try again"
@@ -51,8 +51,8 @@ def main():
 
 
 if __name__ == "__main__":
-    #app.run()
-    app.run(port=8000, host='0.0.0.0')
+    app.run()
+    #app.run(port=8000, host='0.0.0.0')
 
 
 
