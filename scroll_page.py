e5fb8bc514671af8a8255d20e1f2484ea0167740
abhay <akyadav7020@gmail.com>
2022-09-16 01:01:04 +0530

    saved

--- scroll_page.py
+++ scroll_page.py
@@ -8,14 +8,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 import time
 
 def Scroll_Page(link,i):
     try:
 
-        option = Options()
-        option.headless = True
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--hdisable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get(link)
         time.sleep(5)
         for k in range(i):
---
+++ test.py
@@ -0,0 +1,9 @@
+
+
+
+l=["https://www.youtube.com/shorts/ogyla43oL5A",
+   "https://www.youtube.com/watch?v=E0khpjVNzpY"]
+
+for i in l:
+    print("shorts" in i)
+
